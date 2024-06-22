13199370ccac506536c2a38c143a36c3078c8f58
Augustine Blewudzi <40745784+AugustineBlewu1@users.noreply.github.com>
2024-06-22 21:29:06 +0000

    with console

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   esbuild: {
-    drop: ['console', 'debugger'], //comment this if you want to see logs
-  },
+  //  esbuild: {
+  //   drop: ['console', 'debugger'], //comment this if you want to see logs
+  // },
+  
 })
